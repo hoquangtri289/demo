f31a2c135068a931994d3dbc6d16c5760a398d60
Hồ Quang Trí <hoquangtri289@hotmail.com>
2020-07-06 08:41:36 +0700

    Edit: doi ten index.js den server.js

--- server.js
+++ server.js
@@ -5,4 +5,11 @@ app.get('/', (req, res) => {
     res.json({message: 'hello app of me'});
 })
 
+app.get('/admin', (req, res) => {
+    res.json({
+        message: 'Hello to admin',
+        username: 'username',
+        password: 'password'
+    })
+})
 app.listen(port, () => console.log('server on run'));
